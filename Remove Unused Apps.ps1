ac9856da418d865bcac308e8d52f8aefe711b3e0
Dinesh <dineshmike06@gmail.com>
2026-01-05 11:16:25 +0800

    Added script to remove Microsoft 365 Copilot

--- Remove Unused Apps.ps1
+++ Remove Unused Apps.ps1
@@ -2,6 +2,8 @@ Get-AppxPackage *Windows.DevHome* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.YourPhone* | Remove-AppxPackage
 Get-AppxPackage *Microsoft.OutlookForWindows* | Remove-AppxPackage
+Get-AppxPackage -AllUsers -Name "Microsoft.Copilot" | Remove-AppxPackage -AllUsers
+Get-AppxPackage -AllUsers -Name "Microsoft.MicrosoftOfficeHub*" | Remove-AppxPackage -AllUsers
 winget uninstall "Windows Web Experience Pack"
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*Windows.DevHome*' | Remove-AppxProvisionedPackage -Online
 Get-AppxProvisionedPackage -Online | ? DisplayName -like '*Microsoft.GetHelp*' | Remove-AppxProvisionedPackage -Online
